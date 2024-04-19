768f2c5805b3accc894a99dcefc208c15ae5d5cb
Marcelo Silva <marcelo.cansilva@gmail.com>
2024-04-19 14:23:17 -0300

    Add comments user, error save

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :user_comments, dependent: :destroy
+  
   has_one_attached :photo
 
   enum access: { user: 0, admin: 1 }
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   end
 
   resources :posts, only: [:index, :show]
+  resources :user_comments, only: [:new, :create, :destroy]
 end
